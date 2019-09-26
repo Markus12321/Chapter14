76a703183a9eee3b4a5f03752809112289d7bb4c
Markus B <Markus1abc@gmail.com>
2019-09-26 10:09:24 +0200

    Created non function GUI

---
+++ 14.7/14.7/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace _14._7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorGroupBox = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.sizeGroupBox = new System.Windows.Forms.GroupBox();
+            this.drawingPanel = new System.Windows.Forms.Panel();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.colorGroupBox.SuspendLayout();
+            this.sizeGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // colorGroupBox
+            // 
+            this.colorGroupBox.Controls.Add(this.radioButton4);
+            this.colorGroupBox.Controls.Add(this.radioButton3);
+            this.colorGroupBox.Controls.Add(this.radioButton2);
+            this.colorGroupBox.Controls.Add(this.radioButton1);
+            this.colorGroupBox.Location = new System.Drawing.Point(51, 27);
+            this.colorGroupBox.Name = "colorGroupBox";
+            this.colorGroupBox.Size = new System.Drawing.Size(137, 167);
+            this.colorGroupBox.TabIndex = 0;
+            this.colorGroupBox.TabStop = false;
+            this.colorGroupBox.Text = "Color";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(19, 122);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(52, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Black";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(19, 89);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(54, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Green";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(19, 54);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(46, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Blue";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(19, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(45, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Red";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // sizeGroupBox
+            // 
+            this.sizeGroupBox.Controls.Add(this.radioButton7);
+            this.sizeGroupBox.Controls.Add(this.radioButton6);
+            this.sizeGroupBox.Controls.Add(this.radioButton5);
+            this.sizeGroupBox.Location = new System.Drawing.Point(51, 212);
+            this.sizeGroupBox.Name = "sizeGroupBox";
+            this.sizeGroupBox.Size = new System.Drawing.Size(137, 159);
+            this.sizeGroupBox.TabIndex = 1;
+            this.sizeGroupBox.TabStop = false;
+            this.sizeGroupBox.Text = "Size";
+            // 
+            // drawingPanel
+            // 
+            this.drawingPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.drawingPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.drawingPanel.Location = new System.Drawing.Point(256, 27);
+            this.drawingPanel.Name = "drawingPanel";
+            this.drawingPanel.Size = new System.Drawing.Size(515, 372);
+            this.drawingPanel.TabIndex = 2;
+            this.drawingPanel.SizeChanged += new System.EventHandler(this.Resize_Form);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(19, 36);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(50, 17);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Small";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(19, 75);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(62, 17);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Medium";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(19, 114);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(52, 17);
+            this.radioButton7.TabIndex = 6;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Large";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 430);
+            this.Controls.Add(this.drawingPanel);
+            this.Controls.Add(this.sizeGroupBox);
+            this.Controls.Add(this.colorGroupBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Resize_Form);
+            this.SizeChanged += new System.EventHandler(this.Resize_Form);
+            this.colorGroupBox.ResumeLayout(false);
+            this.colorGroupBox.PerformLayout();
+            this.sizeGroupBox.ResumeLayout(false);
+            this.sizeGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox colorGroupBox;
+        private System.Windows.Forms.GroupBox sizeGroupBox;
+        private System.Windows.Forms.Panel drawingPanel;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+    }
+}
+
---
+++ 14.7/14.7/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14._7
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Resize_Form(object sender, EventArgs e)
+        {
+            int formWidth = this.Width;
+
+            colorGroupBox.Left = sizeGroupBox.Left = (int)(formWidth * 0.03);
+            colorGroupBox.Width = sizeGroupBox.Width = (int)(formWidth * 0.14);
+
+            drawingPanel.Left = (int)(formWidth * 0.2);
+            drawingPanel.Width = (int)(formWidth*0.73);
+        }
+
+    }
+}
